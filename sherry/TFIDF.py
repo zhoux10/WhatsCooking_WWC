0af41ee20eb8032bccf1bf7f54d9ab2b67705679
Sherry Zhou <sherry@arcadiadata.com>
2016-05-19 06:11:35 -0700

    Update model
    - Use full prediction set for determining recipe's model (duh...)
    - Reconfirm findings for when higher success rate

--- sherry/TFIDF.py
+++ sherry/TFIDF.py
@@ -1,6 +1,10 @@
 # Based on tutorial from:
 # http://radimrehurek.com/data_science_python/
 
+# TODO: Find out percentage of false positives, figure out how to filter out
+# TODO: Use Pipeline
+# TODO: Use built-in splitter for training/testing?
+
 import matplotlib.pyplot as plt
 import csv
 import pandas
@@ -20,8 +24,7 @@ from sklearn.svm import SVC
 import re
 from nltk.corpus import stopwords # Import the stop word list
 
-
-train_file = 4
+train_file = 0
 train_file_name = "./data/train_%d.json" % train_file
 test_file = (train_file + 1) % 4
 test_file_name = "./data/train_%d.json" % test_file
@@ -91,7 +94,7 @@ recipes_bow = bow_transformer.transform(recipes['ingredients'])
 # print('number of non-zeros:', recipes_bow.nnz)
 # print('sparsity: %.2f%%' % (100.0 * recipes_bow.nnz / (recipes_bow.shape[0] * recipes_bow.shape[1])))
 
-tfidf_transformer = TfidfTransformer(smooth_idf=False).fit(recipes_bow)
+tfidf_transformer = TfidfTransformer().fit(recipes_bow)
 recipes_tfidf = tfidf_transformer.transform(recipes_bow)
 recipe_labeler = MultinomialNB(alpha=0).fit(recipes_tfidf, recipes['cuisine'])
 
@@ -102,23 +105,54 @@ failed_recipes_list = []
 with open(train_file_name) as file:
     recipes_train_json = json.load(file)
 
+def get_prediction(predictions, probability):
+    results = {}
+    cuisine_to_idx = {}
+    max_values = {
+        "name": "",
+        "value": 0
+    }
+
+    for idx, cuis in enumerate(recipe_labeler.classes_):
+        cuisine_to_idx[cuis] = idx
+
+    for idx, pred in enumerate(predictions):
+        cuisine_idx = cuisine_to_idx[pred]
+        cuisine_prob = probability[idx][cuisine_idx]
+        if cuisine_prob >= 0.4:
+            try:
+                results[pred]
+                results[pred] = results[pred] + cuisine_prob
+            except Exception as e:
+                results[pred] = cuisine_prob
+            if results[pred] > max_values["value"]:
+                max_values["name"] = pred
+                max_values["value"] = results[pred]
+    return max_values
+
 for test_recipe in recipes_train_json:
     total_recipes = total_recipes + 1
     test_bow = bow_transformer.transform(separate_into_ingredients(separator.join(test_recipe["ingredients"])))
     test_tfidf = tfidf_transformer.transform(test_bow)
-    test_recipe["prediction"] = recipe_labeler.predict(test_tfidf)[0]
-    if test_recipe["prediction"] != test_recipe["cuisine"]:
+    test_recipe["prediction"] = recipe_labeler.predict(test_tfidf)
+    test_recipe["probability"] = recipe_labeler.predict_proba(test_tfidf)
+    test_recipe["final_prediction"] = get_prediction(test_recipe["prediction"], test_recipe["probability"])
+    if test_recipe["final_prediction"]["name"] != test_recipe["cuisine"]:
+        print(test_recipe["cuisine"])
+        print(test_recipe["final_prediction"])
         failed_recipes = failed_recipes + 1
         failed_recipes_list.append({
           "id": test_recipe["id"],
           "cuisine": test_recipe["cuisine"],
           "prediction": test_recipe["prediction"],
+          "probability": test_recipe["probability"],
+          "final_prediction": test_recipe["final_prediction"],
         })
 
 failed_recipe_pandas = pandas.DataFrame(data = failed_recipes_list, columns = ["id", "cuisine", "prediction"])
 with open("data/results.csv", "a") as file:
     output = csv.writer(file)
-    output.writerow([train_file_name, test_file_name, total_recipes, failed_recipes, failed_recipe_pandas.groupby('cuisine').describe(), failed_recipe_pandas.groupby('prediction').describe(), "Fix spacing at beginning"])
+    output.writerow([train_file_name, test_file_name, total_recipes, failed_recipes, failed_recipe_pandas.groupby('cuisine').describe(), failed_recipe_pandas.groupby('prediction').describe(), "Replicate"])
 
 print("Failed: ", failed_recipes)
 print("Percentage: ", failed_recipes/total_recipes)
